327380422c2e02ea1474652bc6c407849e20bacb
brandonfamiano <brandonfamiano@gmail.com>
2023-09-09 17:33:15 -0400

    started upload page

--- src/Components/upload.js
+++ src/Components/upload.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import NavComponent from './nav';
+import "../styles/main.scss";
 function Upload() {
   return (
     <div>
-      <h1>Upload Page</h1>
+        <NavComponent></NavComponent>
+        <div className="comment">
+          <h3>UPLOAD VIDEO</h3>
+          <div className="comment__form">
+            <div className="comment__form--name">
+              <input type="text" id="name" placeholder="Add a title to your video" />
+            </div>
+            <div className="comment__form--text">
+              <textarea id="comment"  placeholder="Add a description to your video"></textarea>
+            </div>
+          </div>
+          <button id="submit">UPLOAD</button>
+          <Link to="/">Cancel</Link>
+        </div>
     </div>
   );
 }
